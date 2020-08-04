be75ac68b0f787954abd94ee7dce149520800321
0ixit <malviyadx@gmail.com>
2020-08-04 17:41:10 +0530

    Update scripts to regenerate CAL results

--- run8core_baseline_cal.sh
+++ run8core_baseline_cal.sh
@@ -8,7 +8,7 @@ TRACE_DIR=~/DPC-traces
 
 binary=(bimodal-no-no-no-no-lru-baseline-off-8core)
 
-traces=(623.xalancbmk_s-10B.champsimtrace.xz 605.mcf_s-994B.champsimtrace.x z602.gcc_s-2226B.champsimtrace.xz
+traces=(623.xalancbmk_s-10B.champsimtrace.xz 605.mcf_s-994B.champsimtrace.xz 602.gcc_s-2226B.champsimtrace.xz
 	641.leela_s-1052B.champsimtrace.xz 648.exchange2_s-72B.champsimtrace.xz
 	)
 
@@ -25,33 +25,5 @@ do
 	    mix1="$mix1 ${TRACE_DIR}/${trace}"
 	    ((k++))
 	done
-
     (./bin/${binary} -warmup_instructions ${n_warm}000000 -simulation_instructions ${n_sim}000000 ${option} -traces $mix1) &> results_${num_cores}core_${n_warm}_${n_sim}_baseline/${binary}_${trace}.txt &
-
-done
-
-traces=(623.xalancbmk_s-10B.champsimtrace.xz 605.mcf_s-1644B.champsimtrace.xz)
-
-trace_fitting=641.leela_s-1052B.champsimtrace.xz
-
-for ((j=0; j<${#traces[@]}; j++))
-do 	
-
-	k=0
-	mix1=""
-	trace=${traces[$j]}
-	while [ $k -lt 4 ]
-	do
-	    mix1="$mix1 ${TRACE_DIR}/${trace}"
-	    ((k++))
-	done
-
-	while [ $k -lt 8 ]
-	do
-	    mix1="$mix1 ${TRACE_DIR}/${trace_fitting}"
-	    ((k++))
-	done
-
-    (./bin/${binary} -warmup_instructions ${n_warm}000000 -simulation_instructions ${n_sim}000000 ${option} -traces $mix1) &> results_${num_cores}core_${n_warm}_${n_sim}_baseline/${trace}_${trace_fitting}.txt &
-
 done
--- run8core_enclave_cal.sh
+++ run8core_enclave_cal.sh
@@ -8,52 +8,52 @@ TRACE_DIR=~/DPC-traces
 
 binary=(bimodal-no-no-no-no-lru-enclave-on-8core)
 
-traces=(623.xalancbmk_s-10B.champsimtrace.xz 605.mcf_s-994B.champsimtrace.xz 602.gcc_s-2226B.champsimtrace.xz
-	641.leela_s-1052B.champsimtrace.xz 648.exchange2_s-72B.champsimtrace.xz
-	)
+traces=(623.xalancbmk_s-10B.champsimtrace.xz 605.mcf_s-994B.champsimtrace.xz 602.gcc_s-2226B.champsimtrace.xz)
 
 mkdir -p results_${num_cores}core_${n_warm}_${n_sim}_enclave
 
-# 3 mixes
+# 3 mixes [ET-LT]
+for ((j=0; j<3; j++))
+do 	
+	k=0
+	mix1=""
+	trace=${traces[$j]}
+	while [ $k -lt 8 ]
+	do
+	    mix1="$mix1 ${TRACE_DIR}/${trace} no 2 10 35 40 45"
+	    ((k++))
+	done
+    (./bin/${binary} -warmup_instructions ${n_warm}000000 -simulation_instructions ${n_sim}000000 ${option} -traces $mix1) &> results_${num_cores}core_${n_warm}_${n_sim}_enclave/${trace}_25.txt &
+done
+
+# 3 mixes [EF-LT]
+traces=(623.xalancbmk_s-10B.champsimtrace.xz 605.mcf_s-994B.champsimtrace.xz 602.gcc_s-2226B.champsimtrace.xz)
 
 for ((j=0; j<3; j++))
 do 	
-
 	k=0
 	mix1=""
 	trace=${traces[$j]}
 	while [ $k -lt 8 ]
 	do
-	    mix1="$mix1 ${TRACE_DIR}/${trace} no 2 10 40"
+	    mix1="$mix1 ${TRACE_DIR}/${trace} no 1 25 30"
 	    ((k++))
 	done
-
     (./bin/${binary} -warmup_instructions ${n_warm}000000 -simulation_instructions ${n_sim}000000 ${option} -traces $mix1) &> results_${num_cores}core_${n_warm}_${n_sim}_enclave/${trace}_25.txt &
-
 done
 
-# 3 mixes 
+# 3 mixes  [EF-LF]
+traces=(641.leela_s-1052B.champsimtrace.xz 648.exchange2_s-72B.champsimtrace.xz)
 
-traces=(623.xalancbmk_s-10B.champsimtrace.xz 605.mcf_s-994B.champsimtrace.xz 602.gcc_s-2226B.champsimtrace.xz)
-
-for ((j=0; j<${#traces[@]}; j++))
+for ((j=0; j<3; j++))
 do 	
-
 	k=0
 	mix1=""
 	trace=${traces[$j]}
-	while [ $k -lt 4 ]
-	do
-	    mix1="$mix1 ${TRACE_DIR}/${trace} no 1 25 5"
-	    ((k++))
-	done
-
 	while [ $k -lt 8 ]
 	do
-	    mix1="$mix1 ${TRACE_DIR}/${trace} no 0"
+	    mix1="$mix1 ${TRACE_DIR}/${trace} no 1 10 35"
 	    ((k++))
 	done
-
-    (./bin/${binary} -warmup_instructions ${n_warm}000000 -simulation_instructions ${n_sim}000000 ${option} -traces $mix1) &> results_${num_cores}core_${n_warm}_${n_sim}_enclave/${trace}_5.txt &
-
+    (./bin/${binary} -warmup_instructions ${n_warm}000000 -simulation_instructions ${n_sim}000000 ${option} -traces $mix1) &> results_${num_cores}core_${n_warm}_${n_sim}_enclave/${trace}_25.txt &
 done
